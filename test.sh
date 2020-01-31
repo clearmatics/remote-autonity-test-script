5ec5b26de217eecad9e466791ead60d678a2b167
Aneeque Safdar <as@clearmatics.com>
2020-01-31 14:44:45 +0000

    make executable and add non race test file

--- test.sh
+++ test.sh
@@ -15,41 +15,38 @@ if [ ! -d ${RESULTS} ]; then
   mkdir ${RESULTS}
 fi
 
-echo "Running TestTendermintSuccess with race flag..."
-go test ${TENDERMINT_TEST}/... -run=TestTendermintSuccess                    -v -race -timeout=120m >  ${RESULTS}/success-race.txt
+echo "Running TestTendermintSuccess..."
+go test ${TENDERMINT_TEST}/... -run=TestTendermintSuccess                    -v -timeout=20m >  ${RESULTS}/success.txt
 
-echo "Running TestTendermintOneMalicious with race flag..."
-go test ${TENDERMINT_TEST}/... -run=TestTendermintOneMalicious               -v -race -timeout=120m >  ${RESULTS}/one-malicious-race.txt
+echo "Running TestTendermintOneMalicious..."
+go test ${TENDERMINT_TEST}/... -run=TestTendermintOneMalicious               -v -timeout=20m >  ${RESULTS}/one-malicious.txt
 
-echo "Running TestTendermintSlowConnections with race flag..."
-go test ${TENDERMINT_TEST}/... -run=TestTendermintSlowConnections            -v -race -timeout=120m >  ${RESULTS}/slow-connection-race.txt
+echo "Running TestTendermintSlowConnections..."
+go test ${TENDERMINT_TEST}/... -run=TestTendermintSlowConnections            -v -timeout=20m >  ${RESULTS}/slow-connection.txt
 
-echo "Running TestTendermintLongRun with race flag..."
-go test ${TENDERMINT_TEST}/... -run=TestTendermintLongRun                    -v -race -timeout=120m >  ${RESULTS}/long-run-race.txt
+echo "Running TestTendermintLongRun..."
+go test ${TENDERMINT_TEST}/... -run=TestTendermintLongRun                    -v -timeout=20m >  ${RESULTS}/long-run.txt
 
-echo "Running TestTendermintStopUpToFNodes with race flag..."
-go test ${TENDERMINT_TEST}/... -run=TestTendermintStopUpToFNodes             -v -race -timeout=120m >  ${RESULTS}/stop-up-to-f-nodes-race.txt
+echo "Running TestTendermintStopUpToFNodes..."
+go test ${TENDERMINT_TEST}/... -run=TestTendermintStopUpToFNodes             -v -timeout=20m >  ${RESULTS}/stop-up-to-f-nodes.txt
 
-echo "Running TestTendermintStartStopSingleNode with race flag..."
-go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopSingleNode        -v -race -timeout=120m >  ${RESULTS}/start-stop-single-node-race.txt
+echo "Running TestTendermintStartStopSingleNode..."
+go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopSingleNode        -v -timeout=20m >  ${RESULTS}/start-stop-single-node.txt
 
-echo "Running TestTendermintStartStopFNodes with race flag..."
-go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopFNodes            -v -race -timeout=120m >  ${RESULTS}/start-stop-f-nodes-race.txt
+echo "Running TestTendermintStartStopFNodes..."
+go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopFNodes            -v -timeout=20m >  ${RESULTS}/start-stop-f-nodes.txt
 
-echo "Running TestTendermintStartStopFPlusOneNodes with race flag..."
-go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopFPlusOneNodes     -v -race -timeout=120m >  ${RESULTS}/start-stop-f-plus-one-nodes-race.txt
+echo "Running TestTendermintStartStopFPlusOneNodes..."
+go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopFPlusOneNodes     -v -timeout=20m >  ${RESULTS}/start-stop-f-plus-one-nodes.txt
 
-echo "Running TestTendermintStartStopFPlusTwoNodes with race flag..."
-go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopFPlusTwoNodes     -v -race -timeout=120m >  ${RESULTS}/start-stop-f-plus-two-nodes-race.txt
+echo "Running TestTendermintStartStopFPlusTwoNodes..."
+go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopFPlusTwoNodes     -v -timeout=20m >  ${RESULTS}/start-stop-f-plus-two-nodes.txt
 
-echo "Running TestTendermintStartStopAllNodes with race flag..."
-go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopAllNodes          -v -race -timeout=120m >  ${RESULTS}/start-stop-all-nodes-race.txt
+echo "Running TestTendermintStartStopAllNodes..."
+go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopAllNodes          -v -timeout=20m >  ${RESULTS}/start-stop-all-nodes.txt
 
-echo "Running TestCheckBlockWithSmallFee with race flag..."
-go test ${TENDERMINT_TEST}/... -run=TestCheckBlockWithSmallFee               -v -race -timeout=120m >  ${RESULTS}/check-block-with-small-fee-race.txt
+echo "Running TestCheckBlockWithSmallFee..."
+go test ${TENDERMINT_TEST}/... -run=TestCheckBlockWithSmallFee               -v -timeout=20m >  ${RESULTS}/check-block-with-small-fee.txt
 
-echo "Running TestCheckFeeRedirectionAndRedistribution with race flag..."
-go test ${TENDERMINT_TEST}/... -run=TestCheckFeeRedirectionAndRedistribution -v -race -timeout=120m >  ${RESULTS}/check-fee-redirection-and-redistribution-race.txt
-
-echo "Running Tendermint Tests.."
-go test ${TENDERMINT_TEST}/... -v -timeout=120m > ${RESULTS}/tendermint-test.txt
+echo "Running TestCheckFeeRedirectionAndRedistribution..."
+go test ${TENDERMINT_TEST}/... -run=TestCheckFeeRedirectionAndRedistribution -v -timeout=20m >  ${RESULTS}/check-fee-redirection-and-redistribution.txt
