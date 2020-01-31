aea0a7f31a5c4b98b17931e16b20baa2554c9190
Aneeque Safdar <as@clearmatics.com>
2020-01-31 15:03:44 +0000

    add more detail

--- test.sh
+++ test.sh
@@ -15,38 +15,38 @@ if [ ! -d ${RESULTS} ]; then
   mkdir ${RESULTS}
 fi
 
-echo "Running TestTendermintSuccess..."
+echo "Running TestTendermintSuccess... writing to success.txt"
 go test ${TENDERMINT_TEST}/... -run=TestTendermintSuccess                    -v -timeout=20m >  ${RESULTS}/success.txt
 
-echo "Running TestTendermintOneMalicious..."
+echo "Running TestTendermintOneMalicious... writing to one-malicious.txt"
 go test ${TENDERMINT_TEST}/... -run=TestTendermintOneMalicious               -v -timeout=20m >  ${RESULTS}/one-malicious.txt
 
-echo "Running TestTendermintSlowConnections..."
+echo "Running TestTendermintSlowConnections... writing to slow-connection.txt"
 go test ${TENDERMINT_TEST}/... -run=TestTendermintSlowConnections            -v -timeout=20m >  ${RESULTS}/slow-connection.txt
 
-echo "Running TestTendermintLongRun..."
+echo "Running TestTendermintLongRun... writing to long-run.txt"
 go test ${TENDERMINT_TEST}/... -run=TestTendermintLongRun                    -v -timeout=20m >  ${RESULTS}/long-run.txt
 
-echo "Running TestTendermintStopUpToFNodes..."
+echo "Running TestTendermintStopUpToFNodes... writing to stop-up-to-f-nodes.txt"
 go test ${TENDERMINT_TEST}/... -run=TestTendermintStopUpToFNodes             -v -timeout=20m >  ${RESULTS}/stop-up-to-f-nodes.txt
 
-echo "Running TestTendermintStartStopSingleNode..."
+echo "Running TestTendermintStartStopSingleNode... writing to start-stop-single-node.txt"
 go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopSingleNode        -v -timeout=20m >  ${RESULTS}/start-stop-single-node.txt
 
-echo "Running TestTendermintStartStopFNodes..."
+echo "Running TestTendermintStartStopFNodes... writing to start-stop-f-nodes.txt"
 go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopFNodes            -v -timeout=20m >  ${RESULTS}/start-stop-f-nodes.txt
 
-echo "Running TestTendermintStartStopFPlusOneNodes..."
+echo "Running TestTendermintStartStopFPlusOneNodes... writing to start-stop-f-plus-one-nodes.txt"
 go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopFPlusOneNodes     -v -timeout=20m >  ${RESULTS}/start-stop-f-plus-one-nodes.txt
 
-echo "Running TestTendermintStartStopFPlusTwoNodes..."
+echo "Running TestTendermintStartStopFPlusTwoNodes... writing to start-stop-f-plus-two-nodes.txt"
 go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopFPlusTwoNodes     -v -timeout=20m >  ${RESULTS}/start-stop-f-plus-two-nodes.txt
 
-echo "Running TestTendermintStartStopAllNodes..."
+echo "Running TestTendermintStartStopAllNodes... writing to start-stop-all-nodes.txt"
 go test ${TENDERMINT_TEST}/... -run=TestTendermintStartStopAllNodes          -v -timeout=20m >  ${RESULTS}/start-stop-all-nodes.txt
 
-echo "Running TestCheckBlockWithSmallFee..."
+echo "Running TestCheckBlockWithSmallFee... writing to check-block-with-small-fee.txt"
 go test ${TENDERMINT_TEST}/... -run=TestCheckBlockWithSmallFee               -v -timeout=20m >  ${RESULTS}/check-block-with-small-fee.txt
 
-echo "Running TestCheckFeeRedirectionAndRedistribution..."
+echo "Running TestCheckFeeRedirectionAndRedistribution... writing to check-fee-redirection-and-redistribution.txt"
 go test ${TENDERMINT_TEST}/... -run=TestCheckFeeRedirectionAndRedistribution -v -timeout=20m >  ${RESULTS}/check-fee-redirection-and-redistribution.txt
